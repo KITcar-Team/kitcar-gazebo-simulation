28c3c75e5f905c771bf7757e7720a696775018d0
Konstantin Ditschuneit <kditschuneit@icloud.com>
2019-07-28 19:20:55 +0200

    Added roads + modified generate bash

--- data/roads/generate.sh
+++ data/roads/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ROAD=$1
-python3 $KITCAR_GAZEBO_GENERATION_PATH/road-generator.py $KITCAR_GAZEBO_DATA_PATH/roads/$ROAD/road.xml -o $KITCAR_GAZEBO_DATA_PATH/roads/$ROAD/commonroad.xml  
-python3 $KITCAR_GAZEBO_GENERATION_PATH/gazebo-renderer.py $KITCAR_GAZEBO_DATA_PATH/roads/$ROAD/commonroad.xml -o $KITCAR_GAZEBO_DATA_PATH/roads/$ROAD -m $KITCAR_GAZEBO_DATA_PATH/materials -g $KITCAR_GAZEBO_GENERATION_PATH --force
+ROAD=$KITCAR_GAZEBO_SIM_PATH/data/roads/$1
+python3 $KITCAR_GAZEBO_GENERATION_PATH/road-generator.py $ROAD/road.xml -o $ROAD/commonroad.xml  
+python3 $KITCAR_GAZEBO_GENERATION_PATH/gazebo-renderer.py $ROAD/commonroad.xml -o $ROAD -m $KITCAR_GAZEBO_SIM_PATH/data/materials -g $KITCAR_GAZEBO_GENERATION_PATH --force
 
