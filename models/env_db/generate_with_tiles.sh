2930abb9be9553b417f55b614642806ba58f834f
Konstantin Ditschuneit <kditschuneit@icloud.com>
2019-09-05 17:16:31 +0200

    Added tiles, Created 2m and 1.5m modules

---
+++ models/env_db/generate_with_tiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ROAD=$KITCAR_GAZEBO_SIM_PATH/models/env_db/$1
+python3 $KITCAR_GAZEBO_UTILS_PATH/road-generation/road-generator.py $ROAD/road.xml -o $ROAD/commonroad.xml  
+python3 $KITCAR_GAZEBO_UTILS_PATH/road-generation/gazebo-renderer.py $ROAD/commonroad.xml -o $ROAD -n $1 -g $KITCAR_GAZEBO_UTILS_PATH/road-generation --force --tiles
+
