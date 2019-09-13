7ccc13d929208ccefe59cd8b37214f9794637407
Konstantin Ditschuneit <kditschuneit@icloud.com>
2019-09-13 11:36:06 +0200

    generate.sh script now accepts flags
    generate_with_ ... now obsolent

--- models/env_db/setup_modular.sh
+++ models/env_db/setup_modular.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./generate_with_tiles.sh modular-world/modular_1_5m 1.5
-./generate_with_tiles.sh modular-world/modular_2m 2
+./generate.sh modular-world/modular_1_5m -t 1.5
+./generate.sh modular-world/modular_2m -t 2
