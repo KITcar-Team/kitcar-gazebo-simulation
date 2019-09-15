7812e4a17777ccab245bf9f2e0186819c42ae83d
Konstantin Ditschuneit <kditschuneit@icloud.com>
2019-09-15 22:33:21 +0200

    Fix to modular setup script, Adjusted island junction size

--- models/env_db/setup_modular.sh
+++ models/env_db/setup_modular.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./generate.sh modular-world/modular_1_5m -t 1.5
-./generate.sh modular-world/modular_2m -t 2
+./generate.sh -r modular-world/modular_1_5m -t 1.5
+./generate.sh -r modular-world/modular_2m -t 2
