c505f4419063215c8226fa91dfd20ad05361f37d
Konstantin Ditschuneit <kditschuneit@icloud.com>
2019-10-03 00:11:18 +0200

    Minor path fix

--- models/model_db/dr_drift/generate.sh
+++ models/model_db/dr_drift/generate.sh
@@ -2,7 +2,7 @@
 
 GENERATION=$KITCAR_REPO_PATH/kitcar-gazebo-utils/car_model/generate_dr_drift.py
 INPUT_YAML=$(pwd)/dr_drift.yaml
-OUT_CAM_YAML=$KITCAR_REPO_PATH/kitcar-gazebo-simulation/src/gazebo-simulation/param/simulation_car_specs/dr_drift/camera.yaml
+OUT_CAM_YAML=$KITCAR_REPO_PATH/kitcar-gazebo-simulation/src/gazebo_simulation/param/simulation_car_specs/dr_drift/camera.yaml
 OUT_MODEL_SDF=$(pwd)/model.sdf
 
 #Call generation script, which translates the road.xml into the commonroad language.
