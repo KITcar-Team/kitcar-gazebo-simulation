5c40af09230ae9009f65cbe42cfccc0ed36993e1
Konstantin Ditschuneit <kditschuneit@icloud.com>
2020-01-12 14:10:12 +0100

    Gazebo timer node controls gazebo's max update rate to ensure sensor fps
    Gazebo controls its own update rate, yet sometimes (especially in docker
    containers) sensors are not updated often enough.
    This commit introduces the gazebo_timer_node which reduces the
    max_update_rate of gazebo such that the sensors are updated as expected.
    The control of the gazebo update rate is very rudimentary!

--- src/gazebo_simulation/setup.py
+++ src/gazebo_simulation/setup.py
@@ -5,8 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['car_state'],
+    packages=['car_state', 'gazebo_timer'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
-
