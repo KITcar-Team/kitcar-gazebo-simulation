24799d4b7cd505ea93a917b5d3fe319e4118c90c
Georg Steinebrunner <ueizx@student.kit.edu>
2020-02-05 19:09:03 +0100

    base.polygons to_geometry_msg() changed to return Point32
    this is needed because ros Message/Polygon needs Point32

--- utils/base/polygon.py
+++ utils/base/polygon.py
@@ -8,7 +8,6 @@ Date: 15.11.2019
 import shapely.geometry  # Base class
 import shapely.affinity as affinity
 
-#### Compatible formats ####
 import numpy as np
 import geometry_msgs.msg as geometry_msgs
 from road_generation import schema
@@ -19,29 +18,27 @@ from base.transform import Transform
 
 
 class Polygon(shapely.geometry.polygon.Polygon):
-
     def __init__(self, *args):
-        """ Initialize polygon from @args 
-            
+        """ Initialize polygon from @args
             @args can be one of the following types:
                 - np.array
                 - geometry_msgs/Polygon
                 - Line,Line (interpreted as left and right side (eg. a lane)!)
-            """
-        #Create points out of arguments:
+        """
+        # Create points out of arguments:
         try:
-                args = ([Point(p) for p in args[0]], 0)  # Make tuple
+            args = ([Point(p) for p in args[0]], 0)  # Make tuple
         except:
             pass
 
-        #Try to initialize directly
+        # Try to initialize directly
         try:
             super(Polygon, self).__init__(*args)
             return
         except:
             pass
 
-        #Try to initialize from list of points
+        # Try to initialize from list of points
         try:
             args = [[p.x, p.y, p.z] for p in args[0]]
             super(Polygon, self).__init__(args)
@@ -49,15 +46,16 @@ class Polygon(shapely.geometry.polygon.Polygon):
         except:
             pass
 
-        #Try to initialize from geometry_msgs/Polygon
+        # Try to initialize from geometry_msgs/Polygon
         try:
             super(Polygon, self).__init__(
-                [[p.x, p.y, p.z] for p in args[0].points])
+                [[p.x, p.y, p.z] for p in args[0].points]
+            )
             return
         except:
             pass
 
-        #Try to initialize from two lines
+        # Try to initialize from two lines
         try:
             points = args[1].get_points()
             points.extend(reversed(args[0].get_points()))
@@ -67,9 +65,10 @@ class Polygon(shapely.geometry.polygon.Polygon):
         except:
             pass
 
-        #None of the initializations worked
+        # None of the initializations worked
         raise NotImplementedError(
-            f"Polygon initialization not implemented for {type(args[0])}")
+            f"Polygon initialization not implemented for {type(args[0])}"
+        )
 
     def get_points(self):
         """
@@ -79,16 +78,17 @@ class Polygon(shapely.geometry.polygon.Polygon):
 
     def to_schema_lanelet(self, split_idx=0):
         """
-        Conversion to a schema.lanelet. This is done by splitting the polygon in two parts. The first @split_idx- number of points are considered to be on the right side.
-        The returned lanelet has no line markings. 
+        Conversion to a schema.lanelet. This is done by splitting the polygon
+        in two parts. The first @split_idx- number of points are considered to
+        be on the right side. The returned lanelet has no line markings.
         """
         if split_idx == 0:
-            split_idx = int((len(self.exterior.coords) - 1)/2)
+            split_idx = int((len(self.exterior.coords) - 1) / 2)
 
         right_line = Line(self.exterior.coords[:split_idx])
         left_line = Line(reversed(self.exterior.coords[split_idx:-1]))
 
-        #Create lanelet
+        # Create lanelet
         lanelet = schema.lanelet()
         lanelet.rightBoundary = right_line.to_schema_boundary()
         lanelet.leftBoundary = left_line.to_schema_boundary()
@@ -100,7 +100,9 @@ class Polygon(shapely.geometry.polygon.Polygon):
         Convert to geometry_msgs/Polygon
         """
         msg = geometry_msgs.Polygon()
-        msg.points = [p.to_geometry_msg() for p in self.get_points()]
+        msg.points = [
+            geometry_msgs.Point32(*p.to_numpy()) for p in self.get_points()
+        ]
         return msg
 
     def to_numpy(self):
@@ -115,7 +117,8 @@ class Polygon(shapely.geometry.polygon.Polygon):
             return NotImplemented
 
         transformed = affinity.rotate(
-            self, tf.get_angle(), use_radians=True, origin=[0, 0])
+            self, tf.get_angle(), use_radians=True, origin=[0, 0]
+        )
         transformed = affinity.translate(transformed, tf.x, tf.y, tf.z)
 
         return Polygon(transformed.exterior.coords)
