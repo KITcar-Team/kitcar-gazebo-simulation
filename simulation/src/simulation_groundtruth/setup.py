3490c99d2a2218407bd9b107b1910def8171dbcc
Konstantin Ditschuneit <ufzev@student.kit.edu>
2020-04-20 14:06:42 +0200

    Create GroundtruthNode with a mock, visualization and ROS test
    The GroundtruthNode creates a connection between the road's groundtruth and ROS. This commit additionally comes with a GroundtruthMockNode which advertises a mocked road which is useful for testing. Additionally a visualization allows to visualize the groundtruth in RVIZ.
    There's also a ROS test for the GroundtruthNode and an extensive documentation.

--- simulation/src/simulation_groundtruth/setup.py
+++ simulation/src/simulation_groundtruth/setup.py
@@ -7,6 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=(
         [
+            "groundtruth",
         ]
     ),
     package_dir={"": "src"},
