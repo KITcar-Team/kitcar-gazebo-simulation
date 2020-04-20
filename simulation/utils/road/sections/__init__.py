56d862b7eddb44b5e877f091720b52faffca21b2
Georg Steinebrunner <ueizx@student.kit.edu>
2020-04-20 22:17:57 +0200

    Add CircularArcs and BezierCurves to road.sections
    CircularArcs: Left- and RightCircularArc
    BezierCurves: Quad- and CubicBezier

--- simulation/utils/road/sections/__init__.py
+++ simulation/utils/road/sections/__init__.py
@@ -4,9 +4,15 @@ Collect classes and functions which should be included in the road.sections modu
 """
 from road.sections.obstacle import StaticObstacle  # noqa: 402
 from road.sections.straight_road import StraightRoad  # noqa: 402
+from road.sections.bezier_curve import QuadBezier, CubicBezier  # noqa: 402
+from road.sections.circular_arc import LeftCircularArc, RightCircularArc  # noqa: 402
 from road.sections.road_section import RoadSection  # noqa: 402
 
 __all__ = [
+    "QuadBezier",
+    "CubicBezier",
+    "LeftCircularArc",
+    "RightCircularArc",
     "StaticObstacle",
     "StraightRoad",
     "RoadSection",
--- simulation/utils/road/sections/type.py
+++ simulation/utils/road/sections/type.py
@@ -1,2 +1,11 @@
 """Definition of all road section types."""
 STRAIGHT_ROAD = 0
+""""""  # For documentation
+LEFT_CIRCULAR_ARC = 1
+""""""
+RIGHT_CIRCULAR_ARC = 2
+""""""
+QUAD_BEZIER = 3
+""""""
+CUBIC_BEZIER = 4
+""""""
