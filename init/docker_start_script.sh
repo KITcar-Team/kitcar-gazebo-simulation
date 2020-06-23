46ef076c57a740950554ab2ba87c69349d974ff2
Konstantin Ditschuneit <ufzev@student.kit.edu>
2020-06-23 12:51:44 +0200

    Use gitlab registry focal image in Gitlab CI

--- init/docker_start_script.sh
+++ init/docker_start_script.sh
@@ -4,6 +4,17 @@
 # This script can be used to start an xserver and pulseaudio.
 # Thats usually necessary for Gazebo to work.
 
+# Install necessary packages
+sudo apt-get update && apt-get install -y \
+dbus dbus-x11 libasound2 libasound2-plugins alsa-utils \
+alsa-oss pulseaudio pulseaudio-utils \
+xvfb
+
+export DISPLAY=:1.0
+
+# So that dbus-daemon can create /var/run/dbus/system_bus_socket
+sudo mkdir -p /var/run/dbus
+
 sleep 1
 
 Xvfb :1 -screen 0 1600x1200x16 &
