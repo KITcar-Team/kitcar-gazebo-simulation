aff4d3fa17cfe1c405e37882389553d77cad95d7
Konstantin Ditschuneit <ufzev@student.kit.edu>
2020-07-13 23:15:11 +0200

    Simplify obstacles ci road and path yaml

--- simulation/models/env_db/ci_roads/obstacles.py
+++ simulation/models/env_db/ci_roads/obstacles.py
@@ -1,5 +1,4 @@
 import math
-import random
 
 from simulation.utils.road.road import Road  # Definition of the road class
 from simulation.utils.road.sections import (
@@ -9,18 +8,11 @@ from simulation.utils.road.sections import (
     StaticObstacle,
 )
 
-
-def angle():
-    return math.radians(random.random() * 140 + 40)
-
-
-def radius():
-    return random.random() * 3 + 1
-
-
 road = Road()
 road.append(StraightRoad(length=4))
-road.append(LeftCircularArc(radius=radius(), angle=angle(), obstacles=[StaticObstacle()]))
+road.append(LeftCircularArc(radius=3, angle=math.radians(60), obstacles=[StaticObstacle()]))
 road.append(StraightRoad(length=2, obstacles=[StaticObstacle()]))
-road.append(RightCircularArc(radius=radius(), angle=angle(), obstacles=[StaticObstacle()]))
+road.append(
+    RightCircularArc(radius=2, angle=math.radians(60), obstacles=[StaticObstacle()])
+)
 road.append(StraightRoad(length=2))
