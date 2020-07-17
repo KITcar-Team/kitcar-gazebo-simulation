d55d5c3d5e5cb5dadd4a8d73ea2936de0fad91b2
Konstantin Ditschuneit <ufzev@student.kit.edu>
2020-07-17 17:36:28 +0200

    state_machine: Extract drive states into new lane state machine
    The newly added LaneStateMachine keeps track of the lane in which the car is currently in and if there are collisions or other events.
    This simplifies the other state machines and allows to add more complexity to the evaluation of the driving behavior (e.g. driving onto blocked area).

---
+++ simulation/src/simulation_evaluation/src/state_machine/test/test_lane_state_machine.py
@@ -0,0 +1,106 @@
+"""ParkingStateMachine ModuleTest."""
+
+import unittest
+
+from simulation_evaluation.msg import Speaker as SpeakerMsg
+
+from simulation.src.simulation_evaluation.src.state_machine.state_machines.lane import (
+    LaneStateMachine,
+)
+
+from .test import Test
+
+
+class ModuleTest(Test):
+    """Test if the ParkingStateMachine behaves as expected."""
+
+    def test_left_lane(self):
+        """Test if state machine works as expected drives between the left and right lane."""
+        inputs = [
+            SpeakerMsg.RIGHT_LANE,
+            SpeakerMsg.LEFT_LANE,
+            SpeakerMsg.RIGHT_LANE,
+            SpeakerMsg.LEFT_LANE,
+        ]
+        states = [
+            LaneStateMachine.right,
+            LaneStateMachine.left,
+            LaneStateMachine.right,
+            LaneStateMachine.left,
+        ]
+
+        self.state_machine_assert_on_input(
+            LaneStateMachine(self.callback), inputs, states, 3
+        )
+
+    def test_parking_right(self):
+        """Test if state machine works as expected if car parks right."""
+        inputs = [
+            SpeakerMsg.RIGHT_LANE,
+            SpeakerMsg.PARKING_LOT,
+            SpeakerMsg.RIGHT_LANE,
+        ]
+        states = [
+            LaneStateMachine.right,
+            LaneStateMachine.parking_right,
+            LaneStateMachine.right,
+        ]
+
+        self.state_machine_assert_on_input(
+            LaneStateMachine(self.callback), inputs, states, 2
+        )
+
+    def test_parking_left(self):
+        """Test if state machine works as expected if car parks left."""
+        inputs = [
+            SpeakerMsg.RIGHT_LANE,
+            SpeakerMsg.LEFT_LANE,
+            SpeakerMsg.PARKING_LOT,
+            SpeakerMsg.LEFT_LANE,
+        ]
+        states = [
+            LaneStateMachine.right,
+            LaneStateMachine.left,
+            LaneStateMachine.parking_left,
+            LaneStateMachine.left,
+        ]
+
+        self.state_machine_assert_on_input(
+            LaneStateMachine(self.callback), inputs, states, 3
+        )
+
+    def test_collision(self):
+        """Test if state machine works as expected if car collides."""
+        inputs = [
+            SpeakerMsg.RIGHT_LANE,
+            SpeakerMsg.COLLISION,
+        ]
+        states = [
+            LaneStateMachine.right,
+            LaneStateMachine.collision,
+        ]
+
+        self.state_machine_assert_on_input(
+            LaneStateMachine(self.callback), inputs, states, 1
+        )
+
+    def test_off_road(self):
+        """Test if state machine works as expected if car drives offroad while trying to park in."""
+        inputs = [
+            SpeakerMsg.SPEED_UNLIMITED_ZONE,
+            SpeakerMsg.LEFT_LANE,
+            SpeakerMsg.OFF_ROAD,
+        ]
+        states = [
+            LaneStateMachine.right,
+            LaneStateMachine.left,
+            LaneStateMachine.off_road,
+        ]
+
+        self.state_machine_assert_on_input(
+            LaneStateMachine(self.callback), inputs, states, 2
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
