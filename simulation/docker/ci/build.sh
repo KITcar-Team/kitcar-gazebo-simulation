269b3bb38aadadf570fe294f0c976a4086d536e4
Konstantin Ditschuneit <kditschuneit@icloud.com>
2020-09-22 08:57:00 +0200

    docker: Install additional pip packages in ci image, depending on tag
    Depending on the individual CI jobs, different requirements are necessary.
    These changes allow to create separate docker images for special CI jobs.

--- simulation/docker/ci/build.sh
+++ simulation/docker/ci/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 CI_REGISTRY=$1
 TAG=$2
+
+# If tags start with docs or machine_learning, additional packages and dependencies are installed.
+if [ "$TAG" = "docs*" ] ; then BUILD_ARGS="$BUILD_ARGS --build-arg INSTALL_DOC_PACKAGES=true --build-arg INSTALL_ML_PACKAGES=true"; fi
+if [ "$TAG" = "machine_learning*" ] ; then BUILD_ARGS="$BUILD_ARGS --build-arg INSTALL_ML_PACKAGES=true --build-arg INSTALL_NODE_JS=true"; fi
+
 # Before building the docker image, the init files
 # need to be linked into the scope of the Dockerfile!
 rm -rf init/
 mkdir init
 ln ../../../init/* init/
 # Build the image
-docker build -t $CI_REGISTRY/kitcar/kitcar-gazebo-simulation/ci:$TAG .
+docker build -t $CI_REGISTRY/kitcar/kitcar-gazebo-simulation/ci:$TAG $BUILD_ARGS .
 # Clean up
 rm -rf init/
