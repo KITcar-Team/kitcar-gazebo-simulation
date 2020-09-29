df966cf5404fdd4f3b3550c7859fd7096e3faf26
Konstantin Ditschuneit <kditschuneit@icloud.com>
2020-09-29 12:57:53 +0200

    machine_learning: Test resnet generator

--- simulation/utils/machine_learning/models/test/__main__.py
+++ simulation/utils/machine_learning/models/test/__main__.py
@@ -1,2 +1,7 @@
 # Run all tests in this package.
-from . import test_helper, test_resnet_block, test_wgan_critic  # noqa: F401
+from . import (  # noqa: F401
+    test_helper,
+    test_resnet_block,
+    test_resnet_generator,
+    test_wgan_critic,
+)
