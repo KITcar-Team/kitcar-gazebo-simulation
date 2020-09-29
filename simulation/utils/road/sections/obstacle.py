71d3a1a2fe95a0ebd3ca069f8193f11094c8c500
Georg Steinebrunner <ueizx@student.kit.edu>
2020-09-29 22:53:08 +0200

    road: Fix ParkingObstacles not being placed on road.
    collect static obstacles and parking obstacles from parking area as obstacles

--- simulation/utils/road/sections/obstacle.py
+++ simulation/utils/road/sections/obstacle.py
@@ -1,7 +1,7 @@
 """StaticObstacle on road and ParkingObstacle on ParkingSpot."""
 from dataclasses import dataclass
 
-from simulation.utils.geometry import Point
+from simulation.utils.geometry import Point, Transform, Vector
 from simulation.utils.road.sections.road_element import RoadElementRect
 
 
@@ -12,11 +12,27 @@ class StaticObstacle(RoadElementRect):
 
 
 @dataclass
-class ParkingObstacle(StaticObstacle):
+class _ParkingObstacle(StaticObstacle):
     center: Point = Point(0.2, -0.2)
     """Center point of the obstacle."""
+
+
+@dataclass
+class ParkingObstacle(_ParkingObstacle):
     width: float = 0.15
     """Width of the obstacle."""
     depth: float = 0.15
     """Width of the obstacle."""
     normalize_x: bool = False
+
+    @property
+    def center(self) -> Point:
+        """Point: Center of the element in global coordinates."""
+        tf = Transform([-self._center.x, 0], 0) if self.normalize_x else 1
+        return Point(self.transform * (tf * Vector(self._center)))
+
+    @center.setter
+    def center(self, c: Point):
+        if not type(c) is Point:
+            c = Point(c)
+        self._center = c
