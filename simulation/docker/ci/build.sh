e144cdcf90e2d26e72972e94ed514d8c6b14d845
Konstantin Ditschuneit <kditschuneit@icloud.com>
2021-01-15 00:53:29 +0100

    docker: Ensure repos are available in ci
    To achieve this, a new TMP_KITCAR_REPO_PATH variable is introduced.
    And at the end of building the docker image, all repos within
    /builds/kitcar (which is deleted by gitlab in the ci) are moved
    into a different path (/home/kitcar) and must then be moved back
    into their place in the ci.

--- simulation/docker/ci/build.sh
+++ simulation/docker/ci/build.sh
@@ -2,8 +2,7 @@
 SERVICE=$1
 TAG=$2
 PARENT_TAG=$3
-KITCAR_REPO_PATH_IN_IMAGE=$4
-KITCAR_ROSBAGS_PATH=$5
+KITCAR_ROSBAGS_PATH=$4
 
 # Before building the docker image, the init files
 # need to be linked into the scope of the Dockerfile!
@@ -18,7 +17,6 @@ if [[ ! -z "$KITCAR_ROSBAGS_PATH" ]]; then cp -r $KITCAR_ROSBAGS_PATH ./kitcar-r
 # Set env variable used by docker-compose to create the image's tag
 export CI_IMAGE_TAG=$TAG
 export CI_PARENT_TAG=$PARENT_TAG
-export CI_REPO_PATH=$KITCAR_REPO_PATH_IN_IMAGE
 # Build the image
 docker-compose build $SERVICE
 
