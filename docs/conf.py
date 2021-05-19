d468ce99e25a5417b713cf83bb8f4f5df11c8204
Manuel Raimann <raimannma@outlook.de>
2021-05-19 20:18:02 +0000

    docs: Change lineos style config to table

--- docs/conf.py
+++ docs/conf.py
@@ -109,6 +109,11 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
+# Set lineos style to table
+# See also here:
+# sphinx-doc.org/en/master/usage/configuration.html#confval-html_codeblock_linenos_style
+html_codeblock_linenos_style = "table"
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
